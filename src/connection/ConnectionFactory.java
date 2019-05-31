cb06da0be6a50b195bea82c24624c46843500b5c
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-05-31 00:48:56 -0300

    "Adicionado os métodos de conexão na classe ConnectionFactory"

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -1,7 +1,10 @@
 package connection;
 
-import com.mysql.jdbc.Connection;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -10,11 +13,56 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
     
-    public Connection getConnection() {
-		try {
-			return (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/clube_bd_ap2", "root", "password");
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private static final String DRIVER = "com.mysql.jdbc.Driver"; 
+    private static final String URL = "jdbc:mysql://localhost:3306/club_bd_ap2"+"?useTimezone=true&serverTimezone=UTC";//PROTOCOLO DE COMUNICAÇÃO
+    private static final String USER = "root"; // MEU USUARIO - ALTERAR AQUI
+    private static final String PASS = "password"; // MINHA SENHA - ALTERAR AQUI
+    
+    public static Connection getConnection() {
+        
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Erro na Conexão");
+        }
+        return null;
+}
+    public static void closeConnection(Connection con) {
+        if (con != null) { 
+        try {
+            con.commit();
+            con.close();
+        } catch (SQLException e) {
+            System.err.println("Erro na Conexão");
+            e.getErrorCode();
+         
+        }
+    } 
+    }
+    public static void closeConnection(Connection con,PreparedStatement stat) {
+        if (stat != null) {
+            try {
+                stat.close();
+            } catch (SQLException e) {
+                System.err.println("Erro na Conexão");
+                e.getErrorCode();
+             }
+        }
+     
+        closeConnection(con);
+    }
+    
+    public static void closeConnection(Connection con,PreparedStatement stat,ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                 System.err.println("Erro na Conexão:");
+                 e.getErrorCode();
+               
+                 }
+        }
+        closeConnection(con, stat);
+    }
 }
